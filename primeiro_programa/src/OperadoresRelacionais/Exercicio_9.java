2629adb03d70a93389f6c92ad4ca097dd1c6ccb2
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2023-06-04 15:02:04 -0300

    fim exercicios operadores relacionais

---
+++ primeiro_programa/src/OperadoresRelacionais/Exercicio_9.java
@@ -0,0 +1,22 @@
+package OperadoresRelacionais;
+
+import javax.crypto.spec.PSource;
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class Exercicio_9 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+
+        System.out.println("Digite um numero ");
+        numero = teclado.nextInt();
+        if(numero%2 ==0){
+            System.out.println("è par");
+        }else {
+            System.out.println("impar");
+        }
+    }
+
+
+}
