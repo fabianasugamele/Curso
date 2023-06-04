a7f4b615f5237a60565352749de0a77e70773daa
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2023-06-04 13:30:39 -0300

    variaveis simples
    exercicios ok

---
+++ primeiro_programa/src/Variaveis_Simples/Exercicio_01.java
@@ -0,0 +1,17 @@
+package Variaveis_Simples;
+
+import java.util.Scanner;
+
+public class Exercicio_01 {
+    public static void main(String[] args) {
+        Scanner teclado= new Scanner(System.in);
+        String nome, sobrenome;
+
+        System.out.println("Digite seu nome ");
+        nome = teclado.nextLine();
+        System.out.println("Digite seu sobrenome");
+        sobrenome = teclado.nextLine();
+        System.out.println("Seja bem vindo "+nome+" "+sobrenome);
+
+    }
+}
---
+++ primeiro_programa/src/Variaveis_Simples/Exercicio_02.java
@@ -0,0 +1,15 @@
+ package Variaveis_Simples;
+
+public class Exercicio_02 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        b = a;
+        a = b;
+
+        System.out.println("A = "+a);
+        System.out.println("B = "+b);
+
+    }
+}
