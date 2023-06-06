cb56d20e0d09e7513af96e119d5eeace158cf890
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2023-06-05 23:45:57 -0300

    Create Exercicio_18.java

---
+++ primeiro_programa/src/operadoresLogicos/Exercicio_18.java
@@ -0,0 +1,17 @@
+package operadoresLogicos;
+
+public class Exercicio_18 {
+  /*  18) Faça um algoritmo para ler: o nome do produto, a quantidade
+    adquirida e o preço unitário. Em seguida, ele deverá calcular e
+    escrever o total (total = quantidade adquirida * preço unitário), o
+    desconto e o total a pagar (total a pagar = total - desconto),
+    sabendo-se que:
+            • Se quantidade <= 5 o desconto será de 2%;
+• Se quantidade > 5 e quantidade <=10 o desconto será de 3%;
+• Se quantidade > 10 o desconto será de 5%/*
+   */
+     public static void main(String[] args) {
+         
+
+  }
+}
