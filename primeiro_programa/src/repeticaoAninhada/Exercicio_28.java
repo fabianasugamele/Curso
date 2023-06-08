c851cf3a782db306634072bd15a19575dc843092
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2023-06-08 17:57:41 -0300

    fim estrutura aninhada

---
+++ primeiro_programa/src/repeticaoAninhada/Exercicio_28.java
@@ -0,0 +1,22 @@
+package repeticaoAninhada;
+
+public class Exercicio_28 {
+    public static void main(String[] args) {
+        int linha, coluna=10;
+        int tabuada;
+
+        for (linha = 1;linha<=10;linha++){
+            for (coluna =1;coluna<=10;coluna++){
+                tabuada = linha * coluna;
+                System.out.println(coluna+"*"+linha+"="+tabuada);
+
+            }
+            System.out.println("------");
+
+
+    }
+
+
+
+    }
+}
